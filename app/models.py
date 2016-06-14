3414292b3d508b6e92495742b6d8c5b00f66c729
xdd-xoo <tainshi1412@gmail.com>
2016-06-15 01:13:39 +0800

    second develop flasky app , work for spider app

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from . import db
-
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -15,7 +16,29 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+class OnboardRequest(db.Model):
+    __tablename__ = 'onboard_request'
+    id = db.Column(db.Integer, primary_key=True)
+    software_product = db.Column(db.String(64), unique=True, index=True)
+    requester = db.Column(db.String(24), unique=True)
+    sharepoint_path = db.Column(db.Text())
+    milestone_name = db.Column(db.String(64))
+    test_cycle = db.Column(db.String(24))
+    create_date = db.Column(db.DateTime(), default=datetime.utcnow)
+    active = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return '<Onboard request %r from %r>'%(self.software_product, self.requester)
