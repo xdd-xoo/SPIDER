58ed2944c4cd0cb2ad62b12fcb2805427b63e8e2
xdd-xoo <tainshi1412@gmail.com>
2016-06-16 01:23:10 +0800

    add request form and test form with user model

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,11 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import Required
 
 
-class NameForm(Form):
-    name = StringField('What is your name?', validators=[Required()])
-    submit = SubmitField('Submit')
+class RequestForm(Form):
+    software_product = StringField('Software Product: Such as "MSM8976LA.1.0"', validators=[Required()])
+    requester = StringField('Requester: Like "lxin@qti.qualcomm.com"', validators=[Required()])
+    sharepoint_path = StringField('Sharepoint Path', validators=[Required()])
+    submit = SubmitField('Submit Onboard Request')
+
--- app/main/views.py
+++ app/main/views.py
@@ -1,27 +1,27 @@
-from flask import render_template, session, redirect, url_for, current_app
+from flask import render_template, session, redirect, url_for, current_app,flash
 from .. import db
 from ..models import User
 from ..email import send_email
 from . import main
-from .forms import NameForm
+from .forms import RequestForm
 
 
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/spider', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
+    form = RequestForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
+        user = User.query.filter_by(username=form.requester.data).first()
         if user is None:
-            user = User(username=form.name.data)
+            user = User(username=form.requester.data)
             db.session.add(user)
-            session['known'] = False
-            if current_app.config['FLASKY_ADMIN']:
-                send_email(current_app.config['FLASKY_ADMIN'], 'New User',
-                           'mail/new_user', user=user)
+            flash("New requester %s added successfully"%user.username)
+            #session['known'] = False
+            #if current_app.config['FLASKY_ADMIN']:
+            #    send_email(current_app.config['FLASKY_ADMIN'], 'New User',
+            #               'mail/new_user', user=user)
         else:
-            session['known'] = True
-        session['name'] = form.name.data
+            flash("Requester have been stored!")
+        #    session['known'] = True
+        #session['name'] = form.requester.data
         return redirect(url_for('.index'))
-    return render_template('index.html',
-                           form=form, name=session.get('name'),
-                           known=session.get('known', False))
+    return render_template('index.html',form=form)
